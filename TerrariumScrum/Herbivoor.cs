9c4cbc062308bf2ab24f49cbf72bd3a5809ca729
michiel14 <michielgoyvaerts@hotmail.com>
2016-05-09 15:51:01 +0200

    klasseOrganisme

---
+++ TerrariumScrum/Herbivoor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumScrum
+{
+    public class Herbivoor
+    {
+        public bool Paren(int xPostitie, int yPositie) 
+        {
+
+        }
+
+    }
+}
