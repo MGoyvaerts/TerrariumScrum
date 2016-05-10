70340b71e0dbd708e5d12eef556316ef76091b30
SimonL <simon.lecocq@student.pxl.be>
2016-05-10 09:32:29 +0200

    Klasse Plant

---
+++ TerrariumScrum/Plant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumScrum
+{
+    class Plant: Organisme
+    {
+        public Plant(int xPos, int yPos, int levenskracht, string letter)
+            : base(xPos, yPos, levenskracht, letter)
+        {
+            Levenskracht = 1;
+        }
+    }
+}
