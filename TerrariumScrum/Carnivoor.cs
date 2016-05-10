47318be740d9a44790b260489495d23ed4e45ff5
NickDeBie <nick_de_bie@hotmail.com>
2016-05-10 10:02:16 +0200

    klasse dier carnivoor

---
+++ TerrariumScrum/Carnivoor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumScrum
+{
+    public class Carnivoor : Dier
+    {
+        public Carnivoor(int levenskracht, int xPos, int yPos, string letter)
+            : base(levenskracht, xPos, yPos, letter)
+        {
+
+        }
+    }
+}
