a8a23a2b53c997e5d80c0e1d2d66e3bf1118b9e7
SimonL <simon.lecocq@student.pxl.be>
2016-05-10 12:11:44 +0200

    test

--- TerrariumScrum/Program.cs
+++ TerrariumScrum/Program.cs
@@ -16,30 +16,5 @@ namespace TerrariumScrum
             raster.Afbeelden();
             Console.ReadLine();
         }
-        public void NieuwOrganisme(Organisme organisme, int aantalHerbivoren, string[,] grid)
-        {
-            Random rnd = new Random();
-            int aantalNieuwePlanten = rnd.Next(1, 4);  //Random aantal planten toevoegen
-            while (aantalNieuwePlanten != 0)
-            {
-                InvullenPlantenHerbivorenBijVolgendeDag(grid, organisme.Letter, aantalNieuwePlanten);
-            }
-            while (aantalHerbivoren != 0)
-            {
-                InvullenPlantenHerbivorenBijVolgendeDag(grid, organisme.Letter, aantalHerbivoren);
-            }
-        }
-
-        private void InvullenPlantenHerbivorenBijVolgendeDag(string[,] grid, string letter, int aantal)
-        {
-            Random rnd = new Random();
-            int rndRij = rnd.Next(0, 5);
-            int rndKolom = rnd.Next(0, 5);
-            if (grid[rndRij, rndKolom] == ".") //Willekeurige rij en kolom kiezen om na te gaan of deze positie leeg (.) is
-            {
-                grid[rndRij, rndKolom] = letter;
-            }
-            aantal--;
-        }
     }
 }
