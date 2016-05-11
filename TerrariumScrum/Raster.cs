12ba6e5264922673d248bfffd3df3e0323b19346
Caspernicus <kurkiewicz23@gmail.com>
2016-05-11 11:31:18 +0200

    Voorlopig werkende versie van NieuwOrganismeInvullen.. method

--- TerrariumScrum/Raster.cs
+++ TerrariumScrum/Raster.cs
@@ -141,32 +141,39 @@ namespace TerrariumScrum
             Random rnd = new Random();
             double rasterplaats = 0;
             List<Double> rasterplaatsLijst = new List<double>();
+
             for (int i = 0; i < aantal; i++)
             {
                 for (double rij = 0; rij < 6; rij++)       //We gaan alle lege plaatsen in het raster (GeenOrganisme) opslaan in een lijst.
                 {
                     for (double kolom = 0; kolom < 6; kolom++)
                     {
-                        if (raster[(int)rij,(int)kolom].GetType() == typeof(GeenOrganisme))
+                        if (raster[(int)rij, (int)kolom].GetType() == typeof(GeenOrganisme))
                         {
-                            rasterplaats = rij + kolom / 10;
+                            rasterplaats = rij + kolom / 10.0;
                             rasterplaatsLijst.Add(rasterplaats);
                         }
                     }
                 }
-                bool randomIngevuld = false;
-                while (randomIngevuld == false)
+
+                if (rasterplaatsLijst.Count > 0)
                 {
                     double randomLegePlaats = rasterplaatsLijst[rnd.Next(rasterplaatsLijst.Count() - 1)];   //We kiezen een willekeurige lege plaats uit de lijst.
-                    int rij = (int)(randomLegePlaats - randomLegePlaats % 1);
-                    int kolom = (int)((randomLegePlaats % 1.0)*10.0);
-                    grid[rij, kolom] = organisme;
-                    organisme.Rij = rij;
-                    organisme.Kolom = kolom;
-                    randomIngevuld = true;
+                    int _rij = (int)(randomLegePlaats - randomLegePlaats % 1.0);
+                    int _kolom = (int)Math.Round((randomLegePlaats % 1.0) * 10.0);
+                    grid[_rij, _kolom] = organisme;
+                    organisme.Rij = _rij;
+                    organisme.Kolom = _kolom;
+                    rasterplaatsLijst.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("\nHET TERRARIUM KAN NIET VERDER WORDEN OPGEVULD.");
+                    break;
                 }
             }
         }
+
 
         private IOrganisme Opgegeten(Organisme links, Organisme rechts)
         {
