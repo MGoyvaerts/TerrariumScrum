14f42ec99449779a84cb642a3acb6061eebe03ac
NickDeBie <nick_de_bie@hotmail.com>
2016-05-17 09:47:59 +0200

    laatste foutje uit Verplaatsen gehaald en comments bijgezet.

--- TerrariumScrum/Dier.cs
+++ TerrariumScrum/Dier.cs
@@ -24,35 +24,63 @@ namespace TerrariumScrum
         public bool IsVerplaatst = false;
         public IOrganisme[,] Verplaatsen(IOrganisme[,] grid, Dier dier)
         {
-            //de rij & kolom van het dier worden aangepast (en dat dier wordt dan terug gestuurd.)
-            Random random = new Random();
+            //voor dat deze method wordt opgeroepen moet er gecontrolleerd worden of het dier zich mag verplaatsen (geen organisme aan zijn rechterkant).
+            //hierdoor moet er ook apart de methode opgeroepen worden als het dier zich helemaal op de rechterkant bevindt.
+            //Als alle dieren verplaatst zijn moet ook de IsVerplaatst van alle dieren terug op false gezet worden. Dat gebeurd in deze method dus niet!
 
-            int willGetal = 0;
+
+            //*****werking van verplaatsen*****//
+            //comments staan bij elke stap
+
+
+            //voorbereiding op het gebruik van random getallekes.
+            Random random = new Random();
+            int willGetal;
+
+
+            //op de plaats waar het dier nu staat wordt alvast leeg gemaakt (er wordt GeenOrganisme geplaatst.
             grid[dier.Rij, dier.Kolom] = new GeenOrganisme(dier.Rij, dier.Kolom);
 
+
+            //de positie van het dier wordt in variabelen gestopt om verder in de method te gebruiken.
             int rij = dier.Rij;
             int kolom = dier.Kolom;
 
-            while (!dier.IsVerplaatst)//dier verplaatst niet als het in laatste kolom staat
+
+
+            while (!dier.IsVerplaatst)//Eerst wordt gecontroleerd of het dier al verplaatst is of niet.
+            //Deze while is er ook om te zorgen dat het dier zich sowieso verplaatst, 
+            //als er al een dier op de te verplaatsen plek staat komt er een nieuw random getalleke en wordt de lus opnieuw doorlopen
             {
-                willGetal = random.Next(1, 5);
-                
-                switch (willGetal)
+
+                willGetal = random.Next(1, 5);//random getalleke...
+
+                switch (willGetal)//switch op het random getalleke
+                //er zijn 4 random getallen mogelijk.
+                //elk getal stelt een richting voor.
+                //1:rechts   2:links     3:onder    4:boven
+                //in elke case wordt gecontrolleerd of het dier zich niet op de rand van het grid bevind
+                //en zich dan buiten het grid wil verplaatsen.
+                //bv: als het dier helemaal links staat en het random nummerke 2 (om naar links te gaan) wordt gegenereerd,
+                //gebeurd er niets en wordt de whilelus terug opnieuw doorlopen.
+
+                        //Pas als aan alle voorwaarden voldaan zijn wordt de kolom en/of rij van het dier aangepast.
+                //en wordt IsVerplaatst op true gezet.
                 {
-                    case 1:
-                        if (dier.Kolom < 5)
+                    case 1:// verplaatsen naar rechts
+                        if (dier.Kolom < 5)//controle of het dier op de rand rechts staat
                         {
-                            if (!(grid[rij, kolom + 1] is Organisme))
+                            if (!(grid[rij, kolom + 1] is Organisme))// hier wordt gecontroleerd of er geen dier staat op de te verplaatsen plek.
                             {
                                 dier.Kolom += 1;
                                 dier.IsVerplaatst = true;
                             }
-                        }                    
+                        }
                         break;
-                    case 2:
-                        if (dier.Kolom > 0)
+                    case 2://verplaatsen naar links
+                        if (dier.Kolom > 0)//controle of het dier op de rand links staat
                         {
-                            if (!(grid[rij, kolom - 1] is Organisme))
+                            if (!(grid[rij, kolom - 1] is Organisme))// hier wordt gecontroleerd of er geen dier staat op de te verplaatsen plek.
                             {
                                 dier.Kolom -= 1;
                                 dier.IsVerplaatst = true;
@@ -60,20 +88,20 @@ namespace TerrariumScrum
                         }
 
                         break;
-                    case 3:
-                        if (dier.Rij < 5)
+                    case 3://verplaatsen naar onder
+                        if (dier.Rij < 5)//controle of het dier op de rand onder staat
                         {
-                            if (!(grid[rij + 1, kolom] is Organisme))
+                            if (!(grid[rij + 1, kolom] is Organisme))// hier wordt gecontroleerd of er geen dier staat op de te verplaatsen plek.
                             {
                                 dier.Rij += 1;
                                 dier.IsVerplaatst = true;
                             }
                         }
                         break;
-                    case 4:
-                        if (dier.Rij > 0)
+                    case 4://verplaatsen naar boven.
+                        if (dier.Rij > 0)//controle of het dier op de rand boven staat
                         {
-                            if (!(grid[rij - 1, kolom] is Organisme))
+                            if (!(grid[rij - 1, kolom] is Organisme))// hier wordt gecontroleerd of er geen dier staat op de te verplaatsen plek.
                             {
                                 dier.Rij -= 1;
                                 dier.IsVerplaatst = true;
@@ -84,13 +112,11 @@ namespace TerrariumScrum
             }
 
 
-
-
-
+            //het dier met aangepaste positie wordt in het grid gezet.
             grid[dier.Rij, dier.Kolom] = dier;
 
-            dier.IsVerplaatst = true;
-            //Console.WriteLine(willGetal);
+
+            //en vervolgens wordt de aangepaste grid terug gestuurd.
             return grid;
         }
         public Dier Eten(Dier dier, Organisme organisme)
--- TerrariumScrum/Program.cs
+++ TerrariumScrum/Program.cs
@@ -30,22 +30,32 @@ namespace TerrariumScrum
                 if (input == "v")
                 {
                     grid = raster.VolgendeDag(grid);
-                    for (int i = 0; i < 6; i++)
-                    {
+
+
+                    for (int i = 0; i < 6; i++)//2 iteraties om de grid te doorlopen.
+                    { 
                         for (int j = 0; j < 6; j++)
                         {
-                            if (grid[i, j] is Dier)
+                            if (grid[i, j] is Dier)//controle in het grid of het object een dier is.
                             {
-                                if (j < 5)
+                                if (j < 5)// hier worden dieren in de laatste kolom niet verplaatst. 
+                                    //dit is nodig omdat de kolom naast de laatste kolom niet bestaat en ook niet gecontroleerd kan worden.
+                                    //wordt hieronder wel opgelost.
                                 {
-                                    if (grid[i, j + 1] is GeenOrganisme)
+                                    if (grid[i, j + 1] is GeenOrganisme)// hier wordt gecontroleerd of er rechts naast het dier iets staat.
+                                        //staat er niets (GeenOrganisme) wordt het dier verplaatst anders niet.
                                     {
-                                        grid = ((Dier)grid[i, j]).Verplaatsen(grid, (Dier)grid[i, j]);
+                                        grid = ((Dier)grid[i, j]).Verplaatsen(grid, (Dier)grid[i, j]);//verplaatsen op het object oproepen.
                                     }
-                                }                                                                
+                                }
+                                if (j == 5)//om het dier in de laatste kolom te verplaatsen.
+                                {
+                                    grid = ((Dier)grid[i, j]).Verplaatsen(grid, (Dier)grid[i, j]);//verplaatsen op het object oproepen.
+                                }
                             }
                         }
                     }
+
 
                     //grid isverplaatst resetten
                     for (int i = 0; i < 6; i++)
